afed012d7d9f95a23221890742b0d032716ee227
Alongkon Natphunwat <664259031@webmail.npru.ac.th>
2025-07-14 14:44:31 +0700

    Branch

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.send("Restaurant Restful API");
+  res.send("Restaurant Restful API 555");
 });
 
 app.listen(PORT, () => {
